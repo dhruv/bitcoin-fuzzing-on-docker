72531a45043c7fe38e0da2a1d0044b421b9cb7b5
Dhruv Mehta <856960+dhruv@users.noreply.github.com>
2021-07-04 14:57:47 -0700

    adding bitcoin-seeder to repositories that are run

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:focal
 
 RUN apt update
+RUN apt -y dist-upgrade
 RUN apt install -y git
 RUN apt install -y sudo
 RUN apt install -y vim
@@ -16,6 +17,7 @@ RUN apt install -y libboost-dev
 RUN apt install -y libboost-system-dev
 RUN apt install -y libboost-filesystem-dev
 RUN apt install -y libboost-test-dev
+RUN apt install -y libboost-all-dev
 RUN apt install -y libsqlite3-dev
 RUN apt install -y libminiupnpc-dev
 RUN apt install -y libnatpmp-dev
@@ -24,6 +26,7 @@ RUN apt install -y clang
 RUN apt install -y libdb++-dev
 RUN apt install -y libdb-dev
 RUN apt install -y lldb
+RUN apt install -y libssl-dev
 
-RUN mkdir -p /bitcoin /qa-assets
+RUN mkdir -p /bitcoin /qa-assets /bitcoin-seeder
 
--- run-container.sh
+++ run-container.sh
@@ -8,11 +8,16 @@ if [[ -z "${QA_ASSETS_REPO}" ]]; then
     echo "Please set \$QA_ASSETS_REPO"
     exit 0
 fi
+if [[ -z "${BITCOIN_SEEDER_REPO}" ]]; then
+    echo "Please set \$BITCOIN_SEEDER_REPO"
+    exit 0
+fi
+
 
 # maps bitcoin repo core repo to /bitcoin
 # maps qa-assets repo to /qa-assets
 # Uses 3GB ram
 # Uses 3 cpu cores
 # Ptrace and security options allow lldb to work inside docker
-docker run -i -t -v ${BITCOIN_REPO}:/bitcoin -v ${QA_ASSETS_REPO}:/qa-assets -m 3g --cpus=3 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined bitcoin-fuzzing bash
+docker run -i -t -v ${BITCOIN_REPO}:/bitcoin -v ${BITCOIN_SEEDER_REPO}:/bitcoin-seeder -v ${QA_ASSETS_REPO}:/qa-assets -m 3g --cpus=3 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined bitcoin-fuzzing bash
 
